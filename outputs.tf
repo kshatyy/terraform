4db04f3877f101bc2bbf0a363bfe645f7a183737
kshatyy <147355901+kshatyy@users.noreply.github.com>
2024-05-28 10:45:50 +0700

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "all_vms" {
-  value = flatten([
-    [for i in yandex_compute_instance.centos7 : {
-      name = i.name
-      ip_external   = i.network_interface[0].nat_ip_address
-      ip_internal = i.network_interface[0].ip_address
-    }],
-  ])
-}
